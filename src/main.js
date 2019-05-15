9bd25c2b8c1b0951aff45e4f1645f0b344eaa251
LAPTOP-7SSMQJ27\86156 <mcchen1024@163.com>
2019-05-15 15:11:08 +0800

    完整引入Ant-design

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 完整引入and-design
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+Vue.use(Antd)
 
 Vue.config.productionTip = false
 
